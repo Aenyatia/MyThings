2552b176a1cb8b0b8333eaca5d296324ba93ef95
Aenyatia Nirvalen <aenyatia@gmail.com>
2018-11-16 23:01:56 +0100

    Add project with tests.

---
+++ MyThings.Tests/Services/CategoryServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MyThings.Tests.Services
+{
+	public class CategoryServiceTests
+	{
+		[Fact]
+		public void Test()
+		{
+			true.Should().BeTrue();
+		}
+	}
+}
